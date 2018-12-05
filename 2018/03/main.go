f7f968cd1f9c2dd32b4ff050567da01f21c845ed
Manuel de la Peña <manu@wedeploy.com>
2018-12-05 01:19:45 +0100

    Part 1 of Day 3

---
+++ 2018/03/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	log.Println("Advent of code 2018: Day 3")
+
+	inputFile := "input"
+
+	lines, err := readLines(inputFile)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Inches within two or more claims: %v", countClaims(lines))
+}
+
+type square struct {
+	ID     int
+	Left   int
+	Top    int
+	Width  int
+	Height int
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func (s square) plot(plotter [][]int) {
+	for i := s.Left; i < s.totalWidth(); i++ {
+		for j := s.Top; j < s.totalHeight(); j++ {
+			if plotter[i][j] == 0 {
+				plotter[i][j] = 1
+			} else {
+				current := plotter[i][j]
+				plotter[i][j] = current + 1
+			}
+		}
+	}
+}
+
+func (s square) totalHeight() int {
+	return s.Top + s.Height
+}
+
+func (s square) totalWidth() int {
+	return s.Left + s.Width
+}
+
+func countClaims(lines []string) int {
+	maxWidth := 0
+	maxHeight := 0
+	squares := []square{}
+
+	for _, linei := range lines {
+		square := parse(linei)
+
+		squares = append(squares, square)
+
+		if square.totalHeight() >= maxHeight {
+			maxHeight = square.totalHeight()
+		}
+		if square.totalWidth() >= maxWidth {
+			maxWidth = square.totalWidth()
+		}
+	}
+
+	var plotter = initPlotter(maxWidth, maxHeight)
+
+	for _, square := range squares {
+		square.plot(plotter)
+	}
+
+	collitions := 0
+	claims := 0
+	zeros := 0
+	for i, row := range plotter {
+		for j := range row {
+			value := plotter[i][j]
+			if value > 1 {
+				collitions++
+			} else if value == 1 {
+				claims++
+			} else {
+				zeros++
+			}
+		}
+	}
+
+	log.Printf("Using Matrix of (%v, %v): %d.", maxWidth, maxHeight, maxHeight*maxHeight)
+	log.Printf("There were %d claims.", claims)
+	log.Printf("There were %d claims without selection.", zeros)
+	return collitions
+}
+
+func initPlotter(w int, h int) [][]int {
+	var plotter = make([][]int, w)
+	for i := range plotter {
+		plotter[i] = make([]int, h)
+		for j := range plotter[i] {
+			plotter[i][j] = 0
+		}
+	}
+
+	return plotter
+}
+
+func parse(line string) square {
+	// capture ID
+	s := strings.Split(line, "@")
+
+	id := strings.TrimSuffix(s[0], " ")
+	id = strings.TrimPrefix(id, "#")
+
+	i, _ := strconv.Atoi(id)
+
+	// capture paddings
+	paddings := strings.Split(s[1], ":")
+
+	padding := strings.TrimSpace(paddings[0])
+
+	pad := strings.Split(padding, ",")
+
+	left, _ := strconv.Atoi(pad[0])
+	top, _ := strconv.Atoi(pad[1])
+
+	// capture dimensions
+	dimensions := strings.TrimSpace(paddings[1])
+
+	dim := strings.Split(dimensions, "x")
+
+	width, _ := strconv.Atoi(dim[0])
+	height, _ := strconv.Atoi(dim[1])
+
+	return square{
+		ID:     i,
+		Left:   left,
+		Top:    top,
+		Width:  width,
+		Height: height,
+	}
+}
+
+// readLines reads a whole file into memory and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
---
+++ 2018/03/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCountClaims(t *testing.T) {
+	assert := assert.New(t)
+
+	lines, _ := readLines("input")
+	assert.Equal(105231, countClaims(lines))
+}
+
+func TestParse(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := square{
+		ID:     1,
+		Left:   808,
+		Top:    550,
+		Width:  12,
+		Height: 22,
+	}
+	actual := parse("#1 @ 808,550: 12x22")
+
+	assert.Equal(expected.ID, actual.ID)
+	assert.Equal(expected.Left, actual.Left)
+	assert.Equal(expected.Top, actual.Top)
+	assert.Equal(expected.Width, actual.Width)
+	assert.Equal(expected.Height, actual.Height)
+}
+
+func TestParseInput(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := square{
+		ID:     195,
+		Left:   604,
+		Top:    177,
+		Width:  16,
+		Height: 11,
+	}
+	actual := parse("#195 @ 604,177: 16x11")
+
+	assert.Equal(expected.ID, actual.ID)
+	assert.Equal(expected.Left, actual.Left)
+	assert.Equal(expected.Top, actual.Top)
+	assert.Equal(expected.Width, actual.Width)
+	assert.Equal(expected.Height, actual.Height)
+}
